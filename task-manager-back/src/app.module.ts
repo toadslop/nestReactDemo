a4daffec1a4b70ac729bdb7e4aae012759fca95c
Brian Heise <bnheise@gmail.com>
2021-09-05 12:35:00 +0900

    * added typeorm as an import * added connection to the app module constructor
    On branch tasks-module
    Changes to be committed:
    	modified:   src/app.module.ts
    
    Changes not staged for commit:
    	modified:   src/tasks/task.controller.ts
    	modified:   src/tasks/task.entity.ts
    	modified:   src/tasks/task.service.ts
    	modified:   src/tasks/taskTest.http

--- task-manager-back/src/app.module.ts
+++ task-manager-back/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { TaskModule } from './tasks/task.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
